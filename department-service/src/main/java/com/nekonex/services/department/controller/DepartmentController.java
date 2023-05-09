8c395082325c68d6500467aeeeb6e00dda1eff56
Fabrice Chabance <fabrice.chabance@gmail.com>
2023-05-09 21:23:02 +0900

    KMKGS-1 Adding swagger to document the Rest API, Micronaut 3.9.1

--- department-service/src/main/java/com/nekonex/services/department/controller/DepartmentController.java
+++ department-service/src/main/java/com/nekonex/services/department/controller/DepartmentController.java
@@ -14,10 +14,28 @@ import org.slf4j.LoggerFactory;
 import com.nekonex.services.department.client.EmployeeClient;
 import com.nekonex.services.department.model.Department;
 import com.nekonex.services.department.repository.DepartmentRepository;
-
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/api/departments")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Controller",
+				version = "0.0",
+				description = "Nekonex API",
+				license = @License(name = "GPL3.0", url = "https://www.gnu.org/licenses/gpl-3.0.en.html")
+		)
+)
 public class DepartmentController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
@@ -30,12 +48,22 @@ public class DepartmentController {
 		this.employeeClient = employeeClient;
 	}
 
+	@Operation(summary = "Add a department")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Added Department")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Post
-	public Department add(@Body Department department) {
+	public Department add(@Body @RequestBody(description = "Department") Department department) {
 		LOGGER.info("Department add: {}", department);
 		return repository.add(department);
 	}
 
+	@Operation(summary = "Find a department")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "The department that was found")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/{id}")
 	public Department findById(Long id) {
 		LOGGER.info("Department find: id={}", id+1);
@@ -43,18 +71,33 @@ public class DepartmentController {
 		//return null; //test
 	}
 
+	@Operation(summary = "Get all departments")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "All the departments in the system")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get
 	public List<Department> findAll() {
 		LOGGER.info("Department find");
 		return repository.findAll();
 	}
 
+	@Operation(summary = "Find all departments by organization")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "The list of departments matching the specified organization")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/organization/{organizationId}")
 	public List<Department> findByOrganization(Long organizationId) {
 		LOGGER.info("Department find: organizationId={}", organizationId);
 		return repository.findByOrganization(organizationId);
 	}
 
+	@Operation(summary = "Find all departments by organization")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "The list of departments matching the specified organization, this also reset the list of employees in the department by calling the employee service")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/organization/{organizationId}/with-employees")
 	public List<Department> findByOrganizationWithEmployees(HttpRequest<?> request, Long organizationId) {
 		String uuid = request.getAttribute("traceId").toString();
@@ -65,6 +108,11 @@ public class DepartmentController {
 		return departments;
 	}
 
+	@Operation(summary = "Simple Hello endpoint for Authenticated Users")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Hello authenticated user~")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/SecureHello")
 	public String hello() {
 		LOGGER.info("Secure hello");
@@ -72,8 +120,12 @@ public class DepartmentController {
 
 	}
 
-	@Get("/AnonymousHello")
+	@Operation(summary = "Simple Hello endpoint for Authenticated or Anonymous users")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Anonymous hello")
+	})
 	@Secured(SecurityRule.IS_ANONYMOUS)
+	@Get("/AnonymousHello")
 	public String helloAnonymous() {
 		LOGGER.info("Anonymous hello");
 		return "Hello Anonymous";
--- employee-service/src/main/java/com/nekonex/services/employee/controller/EmployeeController.java
+++ employee-service/src/main/java/com/nekonex/services/employee/controller/EmployeeController.java
@@ -13,9 +13,28 @@ import org.slf4j.LoggerFactory;
 
 import jakarta.inject.Inject;
 import java.util.List;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/api/employees")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Employee Controller",
+				version = "0.0",
+				description = "Nekonex API",
+				license = @License(name = "GPL3.0", url = "https://www.gnu.org/licenses/gpl-3.0.en.html")
+		)
+)
 public class EmployeeController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
@@ -23,53 +42,91 @@ public class EmployeeController {
 	@Inject
 	EmployeeRepository repository;
 
+	@Operation(summary = "Add an employee")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Added Employee")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Post
-	public Employee add(@Body Employee employee) {
+	public Employee add(@Body @RequestBody(description = "Employee") Employee employee) {
 		LOGGER.info("Employee add: {}", employee);
 		return repository.add(employee);
 	}
 
+	@Operation(summary = "Find an employee")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found Employee")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/{id}")
 	public Employee findById(Long id) {
 		LOGGER.info("Employee find: id={}", id);
 		return repository.findById(id);
 	}
 
-	@Get("/SecureHello")
-	public String hello() {
-		LOGGER.info("Secure hello");
-		return "Hello authenticated user~";
-	}
-
-	@Get("/AnonymousHello")
-	@Secured(SecurityRule.IS_ANONYMOUS)
-	public String helloAnonymous() {
-		LOGGER.info("Anonymous hello");
-		return "Hello Anonymous";
-	}
-
+	@Operation(summary = "Get all employees")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "All the employees")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get
 	public List<Employee> findAll() {
 		LOGGER.info("Employees find");
 		return repository.findAll();
 	}
 
+	@Operation(summary = "Get all employees for a given department")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "List of employees")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/department/{departmentId}")
 	public List<Employee> findByDepartment(Long departmentId) {
 		LOGGER.info("Employees find: departmentId={}", departmentId);
 		return repository.findByDepartment(departmentId);
 	}
 
+	@Operation(summary = "Get all employees for a given organization")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "List of employees")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/organization/{organizationId}")
 	public List<Employee> findByOrganization(Long organizationId) {
 		LOGGER.info("Employees find: organizationId={}", organizationId);
 		return repository.findByOrganization(organizationId);
 	}
 
+	@Operation(summary = "Get the number of employees handled by the services")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Number of employees")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
 	@Get("/count")
 	long count() {
 		LOGGER.info("Count");
 		return repository.count();
 	}
 
+	@Operation(summary = "Simple Hello endpoint for Authenticated Users")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Hello authenticated user~")
+	})
+	@SecurityRequirement(name = "BearerAuth", scopes = {})
+	@Get("/SecureHello")
+	public String hello() {
+		LOGGER.info("Secure hello");
+		return "Hello authenticated user~";
+	}
+
+	@Operation(summary = "Simple Hello endpoint for Authenticated or Anonymous users")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Anonymous hello")
+	})
+	@Secured(SecurityRule.IS_ANONYMOUS)
+	@Get("/AnonymousHello")
+	public String helloAnonymous() {
+		LOGGER.info("Anonymous hello");
+		return "Hello Anonymous";
+	}
 }
